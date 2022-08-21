033e4d4522be1257d1fbbc5c97af4787d1c0f539
mauroguerra22 <guerramauroemilio22@gmail.com>
2022-08-21 20:53:17 -0300

    agregamos NavBar.Js

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+      <nav>
+        <ul>
+          <li>Inicio</li>
+          <li>Elementos</li>
+          <li>Contacto</li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default NavBar;
+  
